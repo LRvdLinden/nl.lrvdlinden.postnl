6b2f777d6bcb176e74488a2a5839b5de83683067
LRvdLinden <77990847+LRvdLinden@users.noreply.github.com>
2026-09-15 16:48:49 +0200

    Release PostNL for Homey v0.1.5

--- drivers/account/driver.js
+++ drivers/account/driver.js
@@ -4,6 +4,15 @@ const Homey = require('homey');
 
 class PostNLDriver extends Homey.Driver {
   async onPair(session) {
+    session.setHandler('is_authenticated', async () => ({
+      authenticated: this.homey.app.api.hasCredentials(),
+    }));
+    session.setHandler('start_auth', async () => this.homey.app.api.createAuthorization());
+    session.setHandler('complete_auth', async ({ callback }) => {
+      await this.homey.app.api.completeAuthorization(callback);
+      await this.homey.app.api.fetchProfile();
+      return { authenticated: true };
+    });
     session.setHandler('list_devices', async () => {
       if (!this.homey.app.api.hasCredentials()) {
         throw new Error('Log eerst in bij PostNL via Meer → Apps → PostNL → Instellingen.');
